afa413a9928c811feb9b19c49a698610b1c972fb
Rabbit muncher <sszabo1082@gmail.com>
2025-08-21 17:50:41 +1000

    logo

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,6 @@
+<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="17" cy="17" r="16" stroke="black" stroke-width="2" />
+	<circle cx="16.9969" cy="17.0008" r="10.8485" stroke="black" stroke-width="2" />
+	<rect x="1" y="1" width="32" height="32" stroke="black" stroke-width="2" />
+	<rect x="6.14844" y="6.15234" width="21.697" height="21.697" stroke="black" stroke-width="2" />
+</svg>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,38 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
+	import Logo from '$lib/components/Logo.svelte';
 	import { initAirNotice } from 'airnotice-svelte';
 	let { children } = $props();
 
 	initAirNotice({ apiKey: env.PUBLIC_AIRNOTICE_KEY, autoSave: true, showUnitStatus: true });
 </script>
 
-{@render children?.()}
+<div class="app">
+	<header>
+		<Logo />
+	</header>
+	<div class="content">
+		{@render children?.()}
+	</div>
+</div>
 
 <style>
 	:global(body) {
 		font-family: 'Inria Serif';
 		padding: 0;
 		margin: 0;
+	}
+
+	.app {
+		width: 100vw;
+	}
+
+	header {
+		width: 100%;
+		display: flex;
+		position: absolute;
+		top: 20px;
+		align-items: center;
+		justify-content: center;
 	}
 </style>
